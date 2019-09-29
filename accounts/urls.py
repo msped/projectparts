e3f9d55cab357e84a56b16b5ac1922be90e51ead
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-09-29 19:41:12 +0100

    Added reset URLS to account URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from accounts.views import login, logout, register
+from accounts import urls_reset
 
 urlpatterns = [
     url(r'^login/', login, name="login"),
     url(r'^register/', register, name="register"),
     url(r'^logout/', logout, name="logout"),
+    url(r'^password-reset/', include(urls_reset))
 ]
