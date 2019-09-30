67b567ffa1b3ddab79e62d8fa4208c5333f62c6e
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-09-30 23:04:13 +0100

    Added backends to settings for email login

--- projectparts/settings.py
+++ projectparts/settings.py
@@ -102,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTHENTICATION_BACKENDS = [ 
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth',
+]
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
