8873c7b0990f06483912c01bc77b222a10ae9300
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-02 19:55:42 +0100

    Create Profile Model that extends from User

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=11, blank=False)
+    address_line_1 = models.CharField(max_length=40, blank=False)
+    address_line_2 = models.CharField(max_length=40, blank=True)
+    town_city = models.CharField(max_length=40, blank=False)
+    county = models.CharField(max_length=40, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return self.user.email
