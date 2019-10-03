8090731694078960656ce5c679cbc808030e1def
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-03 23:08:06 +0100

    variable / calss name changes for convention

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,19 +2,19 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .models import profile
+from .models import Profile
 
 # Register your models here.
 
 # Admin Registration supplied by https://simpleisbetterthancomplex.com/tutorial/2016/11/23/how-to-add-user-profile-to-django-admin.html
-class profileInLine(admin.StackedInline):
-    model = profile
+class ProfileInLine(admin.StackedInline):
+    model = Profile
     can_delete = False
     verbose_name_plural = 'Profile'
     fk_name = 'user'
 
 class CustomUserAdmin(UserAdmin):
-    inlines = (profileInLine, )
+    inlines = (ProfileInLine, )
 
     def get_inline_instances(self, request, obj=None):
         if not obj:
