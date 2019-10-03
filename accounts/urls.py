d3ccf6d0090b916a72a7747f2f893538a0535b9c
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-03 22:37:54 +0100

    Change profile view to stop conflict with model

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url, include
-from accounts.views import login, logout, register, profile, change_password
+from accounts.views import login, logout, register, profilepage, change_password
 from accounts import urls_reset
 
 urlpatterns = [
     url(r'^login/', login, name="login"),
     url(r'^register/', register, name="register"),
     url(r'^logout/', logout, name="logout"),
-    url(r'^profile/', profile, name="profile"),
+    url(r'^profile/', profilepage, name="profile"),
     url(r'^password-reset/', include(urls_reset)),
     url(r'^change_password/', change_password, name="change_password"),
 ]
