72aaf85c9e477d3bac32e4b868b010c75d4f7182
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-03 20:24:14 +0100

    added change_password route

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
-from accounts.views import login, logout, register
+from accounts.views import login, logout, register, profile, change_password
 from accounts import urls_reset
 
 urlpatterns = [
     url(r'^login/', login, name="login"),
     url(r'^register/', register, name="register"),
     url(r'^logout/', logout, name="logout"),
-    url(r'^password-reset/', include(urls_reset))
+    url(r'^profile/', profile, name="profile"),
+    url(r'^password-reset/', include(urls_reset)),
+    url(r'^change_password/', change_password, name="change_password"),
 ]
