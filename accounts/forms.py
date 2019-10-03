c2b8b431435f1607db1a81040bd7d830f9535f59
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-04 00:26:47 +0100

    Added UserData & ShippingForm for profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -24,13 +24,15 @@ class UserRegisterForm(UserCreationForm):
         fields = ['email', 'username', 'password1', 'password2']
 
     def clean_email(self):
+        """checks if email is already in use"""
         email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
         if User.objects.filter(email=email).exclude(username=username):
             raise forms.ValidationError(u'Email address must be unique')
         return email
-    
+
     def clean_password2(self):
+        """Checks is password match"""
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
 
@@ -41,3 +43,19 @@ class UserRegisterForm(UserCreationForm):
             raise ValidationError("Passwords don't match")
 
         return password2
+
+class UserDataForm(forms.Form):
+    """To change first name, last name, email and phone number"""
+    first_name = forms.CharField(label="First Name", max_length=25)
+    last_name = forms.CharField(label="Last Name", max_length=30)
+    phone_number = forms.CharField(label="Phone Number", max_length=11)
+
+
+class ShippingForm(forms.Form):
+    """To change billing / shipping form"""
+    address_line_1 = forms.CharField(label="Address Line 1", max_length=40)
+    address_line_2 = forms.CharField(label="Address Line 2", max_length=40, required=False)
+    town_city = forms.CharField(label="Town / City", max_length=40)
+    county = forms.CharField(label="County", max_length=40)
+    country = forms.CharField(label="Country", max_length=40)
+    postcode = forms.CharField(label="Postcode", max_length=10)
