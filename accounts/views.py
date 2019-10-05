6a245a9e6f3831c50d0ddee74438fe838ff15c6f
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-05 21:24:28 +0100

    Added profile, with handling of forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import PasswordChangeForm
-from accounts.forms import UserLoginForm, UserRegisterForm
-
-# Create your views here.
+from accounts.forms import UserLoginForm, UserRegisterForm, UserDataForm, ProfileForm, ShippingForm
+from .models import Profile, BillingShipping
 
 def login(request):
+    """Logs a user in / display login page"""
     if request.user.is_authenticated:
         pass # redirect home
 
@@ -27,7 +27,6 @@ def login(request):
         login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
 
-
 @login_required
 def logout(request):
     """Log user out"""
@@ -39,7 +38,7 @@ def register(request):
     """register user"""
     if request.user.is_authenticated:
         return redirect(reverse('index'))
-    
+
     if request.method == 'POST':
         register_form = UserRegisterForm(request.POST)
         if register_form.is_valid():
@@ -61,12 +60,46 @@ def register(request):
     return render(request, 'register.html', {'register_form': register_form})
 
 @login_required
-def profile(request):
-    
-    return render(request, 'profile.html')
+def profilepage(request):
+    """Displays Profile Page / Handles updating of profile"""
+    if request.method == 'POST':
+        if 'user-information' in request.POST:
+            user_form = UserDataForm(request.POST, instance=request.user)
+            profile_form = ProfileForm(request.POST,
+                                       instance=request.user.profile)
+            if user_form.is_valid() and profile_form.is_valid():
+                user_form.save()
+                profile_form.save()
+                messages.success(request, "User Information Updated.")
+                return redirect('profile')
+        else:
+            shipping_form = ShippingForm(request.POST,
+                                         instance=request.user.billingshipping)
+            if shipping_form.is_valid():
+                shipping_form.save()
+                messages.success(request, "User Information Updated.")
+                return redirect('profile')
+    else:
+        user = User.objects.filter(id=request.user.id).first()
+        if user.profile.phone_number == '':
+            user_form = UserDataForm(instance=request.user)
+            profile_form = ProfileForm()
+            shipping_form = ShippingForm()
+        else:
+            user_form = UserDataForm(instance=request.user)
+            profile_form = ProfileForm(instance=request.user.profile)
+            shipping_form = ShippingForm(instance=request.user.billingshipping)
+
+    forms = {
+        'UserDataForm': user_form,
+        'ProfileForm': profile_form,
+        'Shippingform': shipping_form
+    }
+    return render(request, 'profile.html', forms)
 
 @login_required
 def change_password(request):
+    """Displays Change Password Page / Changes password in DB"""
     if request.method == 'POST':
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
@@ -74,8 +107,6 @@ def change_password(request):
             update_session_auth_hash(request, user)
             messages.success(request, 'Password has been updated')
             return redirect('profile')
-        else:
-            messages.error(request, 'There was an error, please try again')
     else:
         form = PasswordChangeForm(request.user)
 
