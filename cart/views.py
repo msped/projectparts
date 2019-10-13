b4e67374ff2132d945fc9e9ce52a8b7866a0b7ca
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-13 20:19:29 +0100

    Added add_to_cart, view_cart, update_cart

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+
+def view_cart(request):
+    """Renders the cart view"""
+    return render(request, 'cart.html')
+
+def add_to_cart(request, product_id):
+    """Adds specified quantity of a product into the cart"""
+
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+
+    if product_id in cart:
+        cart[product_id] = int(cart[product_id]) + quantity
+    else:
+        cart[product_id] = cart.get(product_id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('home'))
+
+def update_cart(request, product_id):
+    """Updates cart contents"""
+
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[product_id] = quantity
+    else:
+        cart.pop(product_id)
+
+    return redirect(reverse('view_cart'))
