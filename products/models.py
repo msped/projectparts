5aed715fb75297562acefc348d376be94bfa6826
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-17 22:22:40 +0100

    Added __str__ to models

--- products/models.py
+++ products/models.py
@@ -6,6 +6,9 @@ class Categories(models.Model):
     """Model for categories which each product will have"""
     category = models.CharField(max_length=75)
 
+    def __str__(self):
+        return self.category 
+
 class Product(models.Model):
     """Model for each Product"""
     name = models.CharField(default='', max_length=254)
@@ -14,3 +17,6 @@ class Product(models.Model):
     category = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
     ticket_price = models.DecimalField(max_digits=4, decimal_places=2)
     product_price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return self.name
