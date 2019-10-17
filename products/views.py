d1ea5e8cb36fad94390ed2933a0a7b07c7e413bf
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-17 22:40:09 +0100

    Added view for all products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def products_view(request):
+    """Shows all products"""
+    products = Product.objects.filter()
+
+    return render(request, 'products.html', {'products': products})
