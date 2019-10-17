5a923c7620b6acb2f0da9d95365479a84013bfdb
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-17 21:10:47 +0100

    Added Media root to settings

--- projectparts/settings.py
+++ projectparts/settings.py
@@ -138,6 +138,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
