7044432f6dc241b4e39754339b0562f2ec8ec0a1
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-17 21:49:53 +0100

    Created product and categories models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Categories(models.Model):
+    """Model for categories which each product will have"""
+    category = models.CharField(max_length=75)
+
+class Product(models.Model):
+    """Model for each Product"""
+    name = models.CharField(default='', max_length=254)
+    description = models.TextField()
+    img = models.ImageField(default='default.jpg', upload_to='media')
+    category = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
+    ticket_price = models.DecimalField(max_digits=4, decimal_places=2)
+    product_price = models.DecimalField(max_digits=7, decimal_places=2)
