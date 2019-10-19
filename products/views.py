46793438e826fea4ad9eb4d1cb55f4cf82962560
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-19 20:46:10 +0100

    Fixed product_detail view not getting product

--- products/views.py
+++ products/views.py
@@ -10,6 +10,6 @@ def products_view(request):
 
 def product_detail(request, product_id):
     """Shows extra detail on a product"""
-    product = Product.objects.filter(id=product_id)
+    product = Product.objects.get(id=product_id)
 
     return render(request, 'product_detail.html', {'product': product})
