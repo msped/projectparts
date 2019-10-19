2bb9fa01fe3ed787c1128c201ce955f8f2837000
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-19 18:43:04 +0100

    Added Vehicle to Admin

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Categories
+from .models import Product, Categories, Vehicle
 
 # Register your models here.
 
 admin.site.register(Categories)
 admin.site.register(Product)
+admin.site.register(Vehicle)
