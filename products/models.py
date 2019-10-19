ef40dd236de0adec98330774e947a2ed9782e2be
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-19 18:42:54 +0100

    Added Vehicle model to show what products fit

--- products/models.py
+++ products/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Vehicle(models.Model):
+    """Model for Make and model of vehicles"""
+    make = models.CharField(default='', max_length=50)
+    model = models.CharField(default='', max_length=100)
+
+    def __str__(self):
+        return f'{self.make} {self.model}'
+
 class Categories(models.Model):
     """Model for categories which each product will have"""
     category = models.CharField(max_length=75)
@@ -18,7 +26,7 @@ class Product(models.Model):
     ticket_price = models.DecimalField(max_digits=4, decimal_places=2)
     product_price = models.DecimalField(max_digits=7, decimal_places=2)
     product_link = models.URLField(default='#')
+    fits = models.ForeignKey(Vehicle, on_delete=models.DO_NOTHING, null=True)
 
     def __str__(self):
         return self.name
-        
