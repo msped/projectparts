c3b0ccc9b9bb31be40374e624d7a9b5c4a32148b
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-19 17:21:49 +0100

    added URLfield to model for link to website

--- products/models.py
+++ products/models.py
@@ -13,10 +13,11 @@ class Product(models.Model):
     """Model for each Product"""
     name = models.CharField(default='', max_length=254)
     description = models.TextField()
-    img = models.ImageField(default='default.jpg', upload_to='media')
+    img = models.ImageField(default='default.jpg', upload_to='')
     category = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
     ticket_price = models.DecimalField(max_digits=4, decimal_places=2)
     product_price = models.DecimalField(max_digits=7, decimal_places=2)
+    product_link = models.URLField(default='#')
 
     def __str__(self):
         return self.name
