3226cb1119ea62d7ef6fd281df504665d3168882
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-21 21:34:38 +0100

    Added tests for Product apps views

--- products/tests.py
+++ products/tests.py
@@ -38,3 +38,33 @@ class CategoriesModelTests(TestCase):
         categories.save()
 
         self.assertEqual(categories.category, 'Interior')
+
+class ProductAppViewsTest(TestCase):
+    """Test case for views in the Products"""
+
+    def test_products_page_response_200(self):
+        """Test response of the main products page"""
+        response = self.client.get('/tickets/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_specific_product_page(self):
+        """Test response of a product extra detail page"""
+        category = Categories.objects.create(
+            category="Exterior"
+        )
+        vehicle = Vehicle.objects.create(
+            make="Mercedes",
+            model="A Class"
+        )
+        Product.objects.create(
+            name="Test Product",
+            description="Description",
+            img="media/default.jpg",
+            category=category,
+            ticket_price="2.50",
+            product_price="795",
+            product_link="https://www.github.com",
+            fits=vehicle
+        )
+        response = self.client.get('/tickets/1/')
+        self.assertEqual(response.status_code, 200)
