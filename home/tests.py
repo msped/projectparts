91579b43635259c6234b4667d60ad62300b6a6eb
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-22 20:51:00 +0100

    Added test to check home response status code

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class HomeViewTests(TestCase):
+    """Home App Test"""
+    def test_home_page_status_code(self):
+        """Test Home Page Response"""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        
