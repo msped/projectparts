75b704044781e4bdc9c6494b396db1896d6fc842
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-25 18:46:07 +0100

    Added Enteries to cart model to stop error in competition app

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+from competition.models import Competition
 
 # Create your models here.
+
+class Enteries(models.Model):
+    """Model for competition enteries / cart item for registered users"""
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    competition = models.ForeignKey(Competition, on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    ticket_number = models.ForeignKey(Product, on_delete=models.DO_NOTHING, null=True)
+    is_paid = models.BooleanField(default=False)
