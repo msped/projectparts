06194cf435f051dfe299caf4697adb02aa95b48f
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-26 17:44:02 +0100

    Added competition model to admin

--- competition/admin.py
+++ competition/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Competition
 
 # Register your models here.
+
+class competitionAdmin(admin.ModelAdmin):
+    exlcude = ('winner',)
+
+
+admin.site.register(Competition, competitionAdmin)
