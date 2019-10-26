461ef81c1f425c8537233ccfd4d080fffc46a3f3
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-26 14:19:06 +0100

    Fixed errors with migrations in enteries model

--- cart/models.py
+++ cart/models.py
@@ -3,12 +3,16 @@ from django.contrib.auth.models import User
 from products.models import Product
 from competition.models import Competition
 
+
 # Create your models here.
 
 class Enteries(models.Model):
     """Model for competition enteries / cart item for registered users"""
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    competition = models.ForeignKey(Competition, on_delete=models.DO_NOTHING)
-    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
-    ticket_number = models.ForeignKey(Product, on_delete=models.DO_NOTHING, null=True)
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    related_competition = models.OneToOneField(Competition, on_delete=models.DO_NOTHING)
+    product = models.OneToOneField(Product, on_delete=models.DO_NOTHING)
+    ticket_number = models.IntegerField(null=True)
     is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Number: {self.ticket_number} by User: {self.user}'
