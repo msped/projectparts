adf226c1ae3c0be8a111fd66055a589bf856ee6c
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-26 14:19:44 +0100

    Added max_lengths and defaults to competition model

--- competition/models.py
+++ competition/models.py
@@ -1,20 +1,18 @@
 from django.db import models
-from cart.models import Enteries
 
 # Create your models here.
-
 
 class Competition(models.Model):
     """Model for each competition"""
     tickets = models.IntegerField(default=4000)
-    tickets_left = models.IntegerField()
-    question = models.CharField()
-    answer_1 = models.CharField()
-    answer_2 = models.CharField()
-    answer_3 = models.CharField()
-    correct_answer = models.CharField()
+    tickets_left = models.IntegerField(default=4000)
+    question = models.CharField(max_length=200)
+    answer_1 = models.CharField(max_length=200)
+    answer_2 = models.CharField(max_length=200)
+    answer_3 = models.CharField(max_length=200)
+    correct_answer = models.CharField(max_length=200)
     is_active = models.BooleanField(default=False)
-    winner = models.ForeignKey(Enteries, on_delete=models.DO_NOTHING, null=True)
+    winner = models.ForeignKey('cart.Enteries', on_delete=models.DO_NOTHING, null=True)
 
     def __str__(self):
         if self.is_active:
