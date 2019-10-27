acffc866b2f6716367b7be74def9747ff1674e3e
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-27 01:20:58 +0100

    Added default for questions and answers

--- competition/models.py
+++ competition/models.py
@@ -6,11 +6,11 @@ class Competition(models.Model):
     """Model for each competition"""
     tickets = models.IntegerField(default=4000)
     tickets_left = models.IntegerField(default=4000)
-    question = models.CharField(max_length=200)
-    answer_1 = models.CharField(max_length=200)
-    answer_2 = models.CharField(max_length=200)
-    answer_3 = models.CharField(max_length=200)
-    correct_answer = models.CharField(max_length=200)
+    question = models.CharField(max_length=200, default="Question")
+    answer_1 = models.CharField(max_length=200, default="Answer 1")
+    answer_2 = models.CharField(max_length=200, default="Answer 2")
+    answer_3 = models.CharField(max_length=200, default="Answer 3")
+    correct_answer = models.CharField(max_length=200, default="Correct Answer")
     is_active = models.BooleanField(default=False)
     winner = models.ForeignKey('cart.Enteries', on_delete=models.DO_NOTHING, null=True, blank=True)
 
