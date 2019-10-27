392b2732f91efcb746d2ba189a563eb23b3915ae
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-27 20:27:37 +0000

    Added fuction to create competition

---
+++ competition/utlis.py
@@ -0,0 +1,22 @@
+from django.core.mail import mail_admins
+from django.template import loader
+from .models import Competition
+
+def new_competition():
+    """Automatically create new competition when current competition
+    is nearing completion"""
+    Competition().save()
+
+    # Send e-mail to admin to alert of new competition creation
+    message = """Hello,\n\n A new competition has been created ready for when
+    the current competition ends.\n\n Please go to the admin panel to add the
+    question and associated answers.\n\n Regards,\n Project Parts"""
+    mail_admins(
+        'New Competition has been created.',
+        message,
+        fail_silently=True,
+        connection=None,
+        html_message=loader.render_to_string(
+            'admin_email.html'
+        )
+    )
