e70fbfd855d9057e0ef64dbdfa62c4fea6f1983d
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-10-30 20:19:23 +0000

    Added user cart for logged in users

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,22 +1,54 @@
 from django.shortcuts import get_object_or_404
 from products.models import Product
+from .models import Orders
 
 def cart_contents(request):
     """
     Ensures that the cart contents are available when rendering
     every page
     """
-    cart = request.session.get('cart', {})
+    if request.user.is_authenticated:
+        cart_items = []
+        total = 0
+        product_amount = 0
 
-    cart_items = []
-    total = 0
-    product_count = 0
+        cart_tickets = Orders.objects.get(user=request.user.id, is_paid=False)
+        for item in cart_tickets:
+            product = get_object_or_404(Product, pk=item.id)
+            total += product.ticket_price
+            product_amount += 1
+            cart_items.append({
+                'id': product.id,
+                'total': total,
+                'product_amount': product_amount
+            })
 
-    for id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=id)
-        total += quantity * product.price
-        product_count += quantity
-        cart_items.append({'id': id, 'quantity': quantity, 'product': product})
+        return {
+            'cart_items': cart_items,
+            'total': total,
+            'product_count': product_amount
+        }
 
-    return {'cart_items': cart_items, 'total': total, 'product_count': product_count}
+    else:
+        cart = request.session.get('cart', {})
+
+        cart_items = []
+        total = 0
+        product_amount = 0
+
+        for id, quantity in cart.items():
+            product = get_object_or_404(Product, pk=id)
+            total += quantity * product.price
+            product_amount += quantity
+            cart_items.append({
+                'id': id,
+                'quantity': quantity,
+                'product': product
+            })
+
+        return {
+            'cart_items': cart_items,
+            'total': total,
+            'product_count': product_amount
+        }
     
