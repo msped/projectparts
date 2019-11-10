de607dcaabee8462489edc3b9652438073f1e6e6
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-11-10 23:49:51 +0000

    Added payment form for stripe

---
+++ checkout/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from competition.models import Competition
+
+class PaymentForm(forms.Form):
+    """Payment form for stripe"""
+    MONTH_CHOICES = [(i, i) for i in range(1, 13)]
+    YEAR_CHOICES = [(i, i) for i in range(2013, 2075)]
+
+    credit_card_number = forms.CharField(label='Card number', required=False)
+    cvv = forms.CharField(label='Security code (CVV)', required=False)
+    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
