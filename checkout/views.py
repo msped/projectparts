e54ccb1c173d2e72b38261ba4e22eade15e62db0
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-11-12 00:24:20 +0000

    Added checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+from random import randint
+from django.shortcuts import render, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.utils import timezone
+import stripe
+from competition.models import Competition
+from cart.models import Orders
+from .models import Entries
+from .forms import PaymentForm
 
 # Create your views here.
+
+stripe.api_key = settings.STRIPE_SECERT
+
+@login_required
+def checkout(request):
+    """Shows checkout page and handles checkout with stripe / DB changes"""
+    comp = Competition.objects.get(is_active=True)
+    orders = Orders.objects.filter(
+        user=request.user.id,
+        is_paid=False,
+        related_competition=comp.id
+    )
+
+    if request.method == "POST":
+        payment_form = PaymentForm(request.POST)
+        user_answer = request.form['user-answer']
+
+        total = 0
+
+        if payment_form.is_valid():
+            for item in orders:
+                total += item.quantity * item.product.ticket_price
+                item.is_paid = True
+                if user_answer == comp.correct_answer:
+                    item.user_answer_correct = True
+                item.save()
+            try:
+                customer = stripe.Charge.create(
+                    amount=int(total * 100),
+                    curreny="GBP",
+                    description=request.user.email,
+                    card=payment_form.cleaned_data['stripe_id']
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your card has been declined.")
+
+            if customer.paid:
+                if user_answer == comp.correct_answer:
+                    for item in orders:
+                        create = True
+                        while create:
+                            ticket_number = randint(1, comp.tickets)
+                            entry, created = Entries.objects.get_or_create(
+                                user=request.user.id,
+                                competition_entry=comp.id,
+                                product=item.product.id,
+                                ticket_number=ticket_number
+                            )
+                            if created:
+                                create = False
+        else:
+            messages.error(
+                request,
+                "We are unable to take payment from that card"
+            )
+    else:
+        payment_form = PaymentForm()
+
+    content = {
+        'user': request.user,
+        'orders': orders,
+        'comp': comp,
+        'payment_form': payment_form,
+        'pushlishable': settings.STRIPE_PUBLISHABLE
+    }
+    return render(request, 'checkout.html', content)
