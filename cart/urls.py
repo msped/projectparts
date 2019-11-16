fe4941339829b44147b9d7d4192c8439e2ef616f
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-11-16 20:11:08 +0000

    fix error with url variable name

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,6 @@ from .views import view_cart, add_to_cart, update_cart
 
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
-    url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
-    url(r'^adjust/(?P<id>\d+)', update_cart, name='update_cart'),
+    url(r'^add/(?P<product_id>\d+)', add_to_cart, name='add_to_cart'),
+    url(r'^adjust/(?P<product_id>\d+)', update_cart, name='update_cart'),
 ]
