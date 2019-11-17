4cdee78de2ee655d6a27cfeea6d56413813b05c4
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-11-17 22:02:53 +0000

    Updated urls for new views

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from .views import view_cart, add_to_cart, update_cart
+from .views import view_cart, add_to_cart, remove_item, decrease_item, increase_item
 
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
     url(r'^add/(?P<product_id>\d+)', add_to_cart, name='add_to_cart'),
-    url(r'^adjust/(?P<product_id>\d+)', update_cart, name='update_cart'),
+    url(r'^add_one/(?P<order_id>\d+)', increase_item, name='increase_item'),
+    url(r'^remove_one/(?P<order_id>\d+)', decrease_item, name='decrease_item'),
+    url(r'^remove/(?P<order_id>\d+)', remove_item, name='remove_item'),
 ]
