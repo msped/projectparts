2fd83e5a44c68beb91b3ce1b80cdcd6aa2384c01
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-11-17 22:04:57 +0000

    Changed contexts for users when logged in

--- cart/contexts.py
+++ cart/contexts.py
@@ -7,51 +7,26 @@ def cart_contents(request):
     Ensures that the cart contents are available when rendering
     every page
     """
-    if request.user.is_authenticated:
-        cart_items = []
-        total = 0
-        product_amount = 0
+    cart = []
+    total = 0
+    product_amount = 0
 
-        try:
-            cart_tickets = Orders.objects.filter(user=request.user.id, is_paid=False)
-        except cart_tickets.DoesNotExist:
-            return None
-        for item in cart_tickets:
-            product = get_object_or_404(Product, pk=item.product_id)
-            total += product.ticket_price
-            product_amount += 1
-            cart_items.append({
-                'id': product.id,
-                'total': total,
-                'product_amount': product_amount
-            })
-
-        return {
-            'cart_items': cart_items,
+    try:
+        cart_items = Orders.objects.filter(user=request.user.id, is_paid=False)
+    except cart_items.DoesNotExist:
+        return None
+    for item in cart_items:
+        product = get_object_or_404(Product, pk=item.product_id)
+        total += product.ticket_price * item.quantity
+        product_amount += 1
+        cart.append({
+            'id': product.id,
             'total': total,
-            'product_count': product_amount
-        }
+            'product_amount': product_amount
+        })
 
-    else:
-        cart = request.session.get('cart', {})
-
-        cart_items = []
-        total = 0
-        product_amount = 0
-
-        for id, quantity in cart.items():
-            product = get_object_or_404(Product, id=id)
-            total += quantity * product.price
-            product_amount += quantity
-            cart_items.append({
-                'id': id,
-                'quantity': quantity,
-                'product': product
-            })
-
-        return {
-            'cart_items': cart_items,
-            'total': total,
-            'product_count': product_amount
-        }
-    
+    return {
+        'cart_items': cart,
+        'total': total,
+        'product_count': product_amount
+    }
