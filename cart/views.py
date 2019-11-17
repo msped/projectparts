8fc2fd3715a85dff3d2331f6b9cf0cc37c7d3442
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-11-17 22:05:24 +0000

    updated views

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from competition.models import Competition
 from products.models import Product
@@ -6,63 +7,58 @@ from .models import Orders
 
 # Create your views here.
 
+@login_required
 def view_cart(request):
     """Renders the cart view"""
-    return render(request, 'cart.html')
+    orders = Orders.objects.filter(user=request.user.id)
+    return render(request, 'cart.html', {'orders': orders})
 
+@login_required
 def add_to_cart(request, product_id):
     """Adds specified quantity of a product into the cart"""
 
     quantity = int(request.POST.get('quantity'))
 
     comp = Competition.objects.get(is_active=True)
+
     user = User.objects.get(id=request.user.id)
+    product = Product.objects.get(id=product_id)
+    order = Orders.objects.get_or_create(
+        user=user,
+        related_competition=comp,
+        product=product,
+        quantity=quantity
+    )
+    order.save()
+    return redirect(reverse('products'))
 
-    if request.user.is_authenticated:
-        product = Product.objects.get(id=product_id)
-        order = Orders(
-            user=user,
-            related_competition=comp,
-            product=product,
-            quantity=quantity
-        )
-        order.save()
-        return redirect(reverse('products'))
-    else:
-        cart = request.session.get('cart', {})
+@login_required
+def increase_item(request, order_id):
+    """increases cart item by one"""
+    order = Orders.objects.get(id=order_id)
 
-        if product_id in cart:
-            cart[product_id] = int(cart[product_id]) + quantity
-        else:
-            cart[product_id] = cart.get(product_id, quantity)
+    qty = int(order.quantity) + 1
 
-        request.session['cart'] = cart
-        return redirect(reverse('products'))
+    order.quantity = qty
+    order.save()
+    return redirect(reverse('view_cart'))
 
-def update_cart(request, product_id):
-    """Updates cart contents"""
+@login_required
+def decrease_item(request, order_id):
+    """decreases cart item by one"""
+    order = Orders.objects.get(id=order_id)
 
-    quantity = int(request.POST.get('quantity'))
+    qty = int(order.quantity) - 1
 
-    if request.user.is_authenticated:
-        comp = Competition.objects.get(is_active=True)
-        order = Orders.objects.get(
-            user=request.user.id,
-            related_competition=comp.id,
-            product=product_id,
-            is_paid=False
-        )
-        if quantity > 0:
-            order.quantity = quantity
-            order.save()
-        else:
-            order.delete()
-    else:
-        cart = request.session.get('cart', {})
+    order.quantity = qty
+    order.save()
+    return redirect(reverse('view_cart'))
 
-        if quantity > 0:
-            cart[product_id] = quantity
-        else:
-            cart.pop(product_id)
+@login_required
+def remove_item(request, order_id):
+    """Remove an item from the cart"""
 
-        return redirect(reverse('products'))
+    order = Orders.objects.filter(id=order_id)
+    order.delete()
+
+    return redirect(reverse('view_cart'))
