c9e6490f2ff64ed2afb6e13a3dc2909585457a6f
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-11-24 19:11:14 +0000

    Added request to function to get user name and email

--- checkout/utils.py
+++ checkout/utils.py
@@ -2,11 +2,11 @@ from django.core.mail import send_mail
 from django.template import loader
 from django.utils.html import strip_tags
 
-def email_order(user_email, orders, total):
+def email_order(request, orders, total):
     """Send out email to user with the order details"""
     html_email = loader.render_to_string(
         'email_templates/order_complete.html',
-        {'order': orders, 'total': total}
+        {'order': orders, 'total': total, 'user': request.user.first_name}
     )
     message = strip_tags(html_email)
     send_mail(
@@ -15,6 +15,6 @@ def email_order(user_email, orders, total):
         from_email='noreply@projectparts.com',
         fail_silently=True,
         connection=None,
-        recipient_list=[str(user_email)],
+        recipient_list=[str(request.user.email)],
         html_message=html_email
     )
