91885dc8894afb1aca0f4db8645c23f17f24a9f9
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-11-25 23:40:11 +0000

    home view now passes tickets_left to template

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from competition.models import Competition
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    comp = Competition.objects.get(is_active=True)
+
+    content = {
+        'tickets_left': comp.tickets_left
+    }
+    return render(request, 'home.html', content)
