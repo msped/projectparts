f0f804ac40d5aa2095c0d2362087b7124c5a90ee
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-11-26 00:03:28 +0000

    Added view for ajax request to get current ticket amount

--- competition/views.py
+++ competition/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from competition.models import Competition
 
 # Create your views here.
+
+def get_current_ticket_amount(request):
+    """Get tickets left in current competition for homepage"""
+    comp = Competition.objects.get(is_active=True)
+    tickets_left = comp.tickets_left
+    return HttpResponse(tickets_left)
