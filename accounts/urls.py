5ffb15d81708812d0655ad47b825024fabbacb06
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-11-26 23:14:55 +0000

    added orders to accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from accounts.views import login, logout, register, profilepage, change_password
+from accounts.views import login, logout, register, profilepage, change_password, users_orders
 from accounts import urls_reset
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^profile/', profilepage, name="profile"),
     url(r'^password-reset/', include(urls_reset)),
     url(r'^change_password/', change_password, name="change_password"),
+    url(r'^orders/', users_orders, name="users_orders"),
 ]
