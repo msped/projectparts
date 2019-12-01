9158077c956dfd6c8e0728d0446a72263d82bac1
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-01 19:52:16 +0000

    Added manufacturer model to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product, Categories, Vehicle
+from .models import Product, Categories, Vehicle, Manufacturer
 
 # Register your models here.
 
 admin.site.register(Categories)
 admin.site.register(Product)
 admin.site.register(Vehicle)
+admin.site.register(Manufacturer)
