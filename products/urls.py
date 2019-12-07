a278aadeed534c0a2362d4b3740582772e9a6b8f
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-07 22:16:42 +0000

    added search and ajax request urls

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from .views import products_view, product_detail
+from .views import products_view, product_detail, search, get_models, get_gens
 
 urlpatterns = [
     url(r'^$', products_view, name="products"),
-    url(r'^(?P<product_id>\d+)/$', product_detail, name="product_detail")
+    url(r'^(?P<product_id>\d+)/$', product_detail, name="product_detail"),
+    url(r'^search/', search, name="search"),
+    url(r'^models/', get_models, name="get_models"),
+    url(r'^gens/', get_gens, name="get_gens"),
 ]
