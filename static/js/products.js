c548243228e35675be137f34a382d0fb6eaff442
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-07 22:21:11 +0000

    added ajax request to populate search dropdowns

---
+++ static/js/products.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $('#make').on('change', function(){
+        $('.added-value-model').remove();
+        make = $('#make').find(":selected").text();
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: '/tickets/models/',
+            data: {
+                'make': make,
+                'csrfmiddlewaretoken': CSRF_TOKEN
+            },
+            success: function(result){
+                for (var d of result){
+                    var template = '<option class="added-value-model" value="' + d + '">' + d + '</option>'; 
+                    $('#model').append(template);
+                }                
+            }
+        });
+    });
+
+    $('#model').on('change', function(){
+        $('.added-value-generation').remove();
+        make = $('#make').find(":selected").text();
+        model = $('#model').find(":selected").text();
+        $.ajax({
+            type: 'POST',
+            url: '/tickets/gens/',
+            data: {
+                'make': make,
+                'model': model,
+                'csrfmiddlewaretoken': CSRF_TOKEN
+            },
+            success: function(result){
+                for ( var d of result){
+                    var template = '<option class="added-value-generation" value="' + d + '">' + d + '</option>'; 
+                    $('#generation').append(template);
+                }        
+            }
+        });
+    });
+});
