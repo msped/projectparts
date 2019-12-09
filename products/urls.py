24b830e69e5e5fc5e0d506c967105a8222290378
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-09 23:02:04 +0000

    removed serach url

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
-from .views import products_view, product_detail, search, get_models, get_gens
+from .views import products_view, product_detail, get_models, get_gens
 
 urlpatterns = [
     url(r'^$', products_view, name="products"),
     url(r'^(?P<product_id>\d+)/$', product_detail, name="product_detail"),
-    url(r'^search/', search, name="search"),
     url(r'^models/', get_models, name="get_models"),
     url(r'^gens/', get_gens, name="get_gens"),
 ]
