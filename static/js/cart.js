c88f95da083be912fbc9681181864c0f2da5dbc6
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-16 01:15:54 +0000

    updated / fixed formating in cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
     const order_total = $('#order-total');
 
     $('.order-qty').each(function(){
-        console.log(this)
         if ($(this).text() == 1){
-           $(this).closest('td').find('#remove-one-item').removeClass('remove-one-item').css('opacity', '0.5') 
+           $(this).closest('td').find('#remove-one-item').removeClass('remove-one-item').css('opacity', '0.5');
         }
-    })
+    });
 
     $('td').on('click', '.remove-one-item', function(e) {
         order_qty = $(this).closest('td').find('.order-qty');
@@ -14,14 +13,14 @@ $(document).ready(function() {
         $.ajax({
             url:'/cart/remove_one/' + order_id,
             success: function(data){
-                order_qty.text(data['qty']);
-                order_total.text(data['total']);
-                if (data['qty'] == 1) {
-                    order_qty.closest('td').find('#remove-one-item').removeClass('remove-one-item').css('opacity', '0.5')
+                order_qty.text(data.qty);
+                order_total.text(data.total);
+                if (data.qty == 1) {
+                    order_qty.closest('td').find('#remove-one-item').removeClass('remove-one-item').css('opacity', '0.5');
                 }
             }
-        })
-    })
+        });
+    });
 
     $('td').on('click', '.add-one-item', function(e) {
         order_qty = $(this).closest('td').find('.order-qty');
@@ -29,12 +28,12 @@ $(document).ready(function() {
         $.ajax({
             url:'/cart/add_one/' + order_id,
             success: function(data){
-                order_qty.text(data['qty']);
-                order_total.text(data['total']);
-                if (data['qty'] > 1) {
-                    order_qty.closest('td').find('#remove-one-item').addClass('remove-one-item').css('opacity', '1')
+                order_qty.text(data.qty);
+                order_total.text(data.total);
+                if (data.qty > 1) {
+                    order_qty.closest('td').find('#remove-one-item').addClass('remove-one-item').css('opacity', '1');
                 }
             }
-        })
-    })
-})
+        });
+    });
+});
