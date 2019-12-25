da692e00d6fbf2144ef2fea5f9660b973e32d828
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-25 17:46:37 +0000

    added code to stop the product not showing on the cart page

--- static/js/cart.js
+++ static/js/cart.js
@@ -36,4 +36,10 @@ $(document).ready(function() {
             }
         });
     });
+
+    if (parseInt(cart_product_count) > 0 ){
+        $('#product-count').css('display', 'initial');
+    } else {
+        $('#product-count').css('display', 'none');
+    }
 });
