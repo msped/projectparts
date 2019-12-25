db964f544743f5e07c5bb301c93ffedc2614356e
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-25 17:48:27 +0000

    remove product_id variable from URL

--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,7 @@ from .views import view_cart, add_to_cart, remove_item, decrease_item, increase_
 
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
-    url(r'^add/(?P<product_id>\d+)', add_to_cart, name='add_to_cart'),
+    url(r'^add/', add_to_cart, name='add_to_cart'),
     url(r'^add_one/(?P<order_id>\d+)', increase_item, name='increase_item'),
     url(r'^remove_one/(?P<order_id>\d+)', decrease_item, name='decrease_item'),
     url(r'^remove/(?P<order_id>\d+)', remove_item, name='remove_item'),
