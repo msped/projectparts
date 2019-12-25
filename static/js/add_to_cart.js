d43f1541e108bd947538a1243a5bbe4a2f7a28ec
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-25 17:47:48 +0000

    created add_to_cart.js to handle the ajax request

---
+++ static/js/add_to_cart.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $('form').submit(function(e){
+        e.preventDefault();
+
+        var qty = $(this).find('.quantity').val();
+        var product_id = $(this).find('.product_id').val();
+        console.log("qty: " + qty + " Prod id: " + product_id)
+
+        $.ajax({
+            url: '/cart/add/',
+            data: {
+                'qty': qty,
+                'product_id': product_id,
+                'csrfmiddlewaretoken': CSRF_TOKEN
+            },
+            type: 'POST',
+            success: function(d){
+                $('#cart').animate({color: '#00B500'});
+                $('#product-count').css('display', 'initial').text(d.cart_amount);
+                $('#cart').animate({color: 'rgba(0, 0, 0, .5)'}, 750);
+            }
+        })
+    })
+
+    if (parseInt(cart_product_count) > 0 ){
+        $('#product-count').css('display', 'initial');
+    } else {
+        $('#product-count').css('display', 'none');
+    }
+})
