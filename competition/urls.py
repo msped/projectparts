e0b4cbc2e15b2a43ba91ccfe56bb9ca87e1f7117
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-26 21:46:37 +0000

    added winners url

--- competition/urls.py
+++ competition/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .views import get_current_ticket_amount
 
 urlpatterns = [
-    url(r'get_current/', get_current_ticket_amount, name="get_current_tickets")
+    url(r'get_current/', get_current_ticket_amount, name="get_current_tickets"),
+    url(r'winners/', get_current_ticket_amount, name="get_current_tickets"),
 ]
