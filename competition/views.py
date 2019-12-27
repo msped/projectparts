686301eda100dac8dabcbde30d1b84df85b7897a
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-27 19:06:44 +0000

    added winners view with pagination

--- competition/views.py
+++ competition/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.paginator import Paginator
 from competition.models import Competition
 
 # Create your views here.
@@ -8,3 +10,18 @@ def get_current_ticket_amount(request):
     comp = Competition.objects.get(is_active=True)
     tickets_left = comp.tickets_left
     return HttpResponse(tickets_left)
+
+def winners(request):
+    """Page to display previous winners for the competition"""
+
+    previous_comp = Competition.objects.filter(is_active=False, winner__isnull=False)    
+
+    paginator = Paginator(previous_comp, 15)
+    page = request.GET.get('page')
+    prev_comp = paginator.get_page(page)
+
+    context = {
+        'prev_comp': prev_comp
+    }
+
+    return render(request, 'winners.html', context)
