2a473a77e90e7155a2fdddfa19496bc3fce2656d
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-27 01:27:32 +0000

    fixed url for winners page

--- competition/urls.py
+++ competition/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from .views import get_current_ticket_amount
+from .views import get_current_ticket_amount, winners
 
 urlpatterns = [
     url(r'get_current/', get_current_ticket_amount, name="get_current_tickets"),
-    url(r'winners/', get_current_ticket_amount, name="get_current_tickets"),
+    url(r'winners/', winners, name="winners"),
 ]
