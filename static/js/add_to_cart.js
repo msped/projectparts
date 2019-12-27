c06776bd97a0462f2b461b5bc52a92ebb0eca3b2
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-27 23:12:14 +0000

    moved product count into base and on file

--- static/js/add_to_cart.js
+++ static/js/add_to_cart.js
@@ -22,10 +22,4 @@ $(document).ready(function() {
             }
         })
     })
-
-    if (parseInt(cart_product_count) > 0 ){
-        $('#product-count').css('display', 'initial');
-    } else {
-        $('#product-count').css('display', 'none');
-    }
 })
--- static/js/cart.js
+++ static/js/cart.js
@@ -36,10 +36,4 @@ $(document).ready(function() {
             }
         });
     });
-
-    if (parseInt(cart_product_count) > 0 ){
-        $('#product-count').css('display', 'initial');
-    } else {
-        $('#product-count').css('display', 'none');
-    }
 });
