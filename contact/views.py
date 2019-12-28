af41195d8070dee0b1771537bdbb9902e1c4eb1b
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-28 16:43:12 +0000

    added contact view with post for email

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.mail import send_mail
+from projectparts.settings import EMAIL_HOST_USER
+from .forms import ContactForm
 
 # Create your views here.
+
+def contact_view(request):
+    """View contact page"""
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+
+            send_mail(subject, message, email, [EMAIL_HOST_USER])
+
+            data = {
+                'sent': True
+            }
+            return JsonResponse(data)
+        else:
+            data = {
+                'sent': False,
+                'error': "Invalid Form"
+            }
+            return JsonResponse(data)
+
+    else:
+        form = ContactForm()
+
+        content = {
+            'form': form,
+        }
+    return render(request, "contactus.html", content)
+    
