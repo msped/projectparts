6c4df38535b13cb85c2d312a1a53101420a9d61d
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-29 00:17:20 +0000

    add ajax test to cart tests.py

--- cart/tests.py
+++ cart/tests.py
@@ -56,7 +56,10 @@ class CartAppTest(TestCase):
             quantity=1,
             product=product_test
         )
-        self.assertEqual(str(test_name), 'Order 3 - Paid False')
+        self.assertEqual(
+            str(test_name),
+            'Order {} - Paid False'.format(test_name.id)
+        )
 
     def test_cart_page_response_user_logged_in(self):
         """Test cart response for when a user is logged in,
@@ -84,3 +87,72 @@ class CartAppTest(TestCase):
         )
         response = self.client.get('/cart/remove/1', follow=True)
         self.assertIn(b'Ticket(s) Removed.', response.content)
+
+    def test_add_to_cart_view(self):
+        """Test adding a product to the cart"""
+        self.client.post(
+            '/accounts/login/',
+            self.user,
+            follow=True
+        )
+        add_product = self.client.post(
+            '/cart/add/',
+            {
+                'qty': '10',
+                'product_id': '1'
+            }
+        )
+        self.assertJSONEqual(
+            str(add_product.content, encoding='utf8'),
+            {'cart_amount': 1}
+        )
+
+    def test_increase_item_view_for_ajax_request(self):
+        """Test increase an orders quantity by one"""
+        self.client.post(
+            '/accounts/login/',
+            self.user,
+            follow=True
+        )
+        user = User.objects.all().first()
+        comp = Competition.objects.all().first()
+        product = Product.objects.all().first()
+        order = Orders.objects.create(
+            user=user,
+            quantity=1,
+            related_competition=comp,
+            product=product
+        )
+        order.save()
+        add_one_product = self.client.get(
+            '/cart/add_one/' + str(order.id)
+        )
+        self.assertJSONEqual(
+            str(add_one_product.content, encoding='utf8'),
+            {'qty': 2, 'total': '5.00'}
+        )
+
+    def test_decrease_item_view_for_ajax_request(self):
+        """Test dencrease an orders quantity by one"""
+        self.client.post(
+            '/accounts/login/',
+            self.user,
+            follow=True
+        )
+        user = User.objects.all().first()
+        comp = Competition.objects.all().first()
+        product = Product.objects.all().first()
+        order = Orders.objects.create(
+            user=user,
+            quantity=2,
+            related_competition=comp,
+            product=product
+        )
+        order.save()
+        add_one_product = self.client.get(
+            '/cart/remove_one/' + str(order.id)
+        )
+        self.assertJSONEqual(
+            str(add_one_product.content, encoding='utf8'),
+            {'qty': 1, 'total': '2.50'}
+        )
