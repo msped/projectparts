94686b72e64f5a2fee6ca3ab8fa2de6014e12480
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-29 15:44:18 +0000

    Made paymentform fields required

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,8 +5,8 @@ class PaymentForm(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 13)]
     YEAR_CHOICES = [(i, i) for i in range(2016, 2075)]
 
-    credit_card_number = forms.CharField(label='Card number', required=False)
-    cvv = forms.CharField(label='Security code (CVV)', required=False)
-    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
-    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
+    credit_card_number = forms.CharField(label='Card number', required=True)
+    cvv = forms.CharField(label='Security code (CVV)', required=True)
+    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=True)
+    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=True)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
