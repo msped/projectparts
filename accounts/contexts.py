da2057ce49807037f8003244206c9a62a3272e18
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-12-30 17:27:00 +0000

    fixed accounts contexts.py

--- accounts/contexts.py
+++ accounts/contexts.py
@@ -1,15 +1,17 @@
-from django.contrib.auth.models import User
 from .models import Profile
-
 
 def check_profile(request):
     """Check profile is complete"""
 
-    profile = Profile.objects.get(id=request.user.id)
+    if request.user.is_authenticated:
 
-    if profile.phone_number == 'Phone No.' and profile.county == "County":
-        profile_incomplete = True
-    else:
+        profile = Profile.objects.get(id=request.user.id)
+
+        if profile.phone_number == 'Phone No.' and profile.county == "County":
+            profile_incomplete = True
+        else:
+            profile_incomplete = False
+    else: 
         profile_incomplete = False
 
     return {
