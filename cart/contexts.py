9a6bb89504060509f345241d80ee37bc863139c6
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-01-11 20:58:00 +0000

    adjust contexts.py to factor in users who arent logged in

--- cart/contexts.py
+++ cart/contexts.py
@@ -7,26 +7,41 @@ def cart_contents(request):
     Ensures that the cart contents are available when rendering
     every page
     """
-    cart = []
+    cart_items = []
     total = 0
     product_amount = 0
+    cart = request.session.get('cart', {})
+    if request.user.is_authenticated:
+        try:
+            cart = Orders.objects.filter(
+                user=request.user.id,
+                is_paid=False
+            )
+        except cart.DoesNotExist:
+            return None
+        for item in cart:
+            product = get_object_or_404(Product, pk=item.product_id)
+            total += product.ticket_price * item.quantity
+            product_amount += 1
+            cart_items.append({
+                'id': item.id,
+                'quantity': item.quantity,
+                'product': product
+            })
+    else:
 
-    try:
-        cart_items = Orders.objects.filter(user=request.user.id, is_paid=False)
-    except cart_items.DoesNotExist:
-        return None
-    for item in cart_items:
-        product = get_object_or_404(Product, pk=item.product_id)
-        total += product.ticket_price * item.quantity
-        product_amount += 1
-        cart.append({
-            'id': product.id,
-            'total': total,
-            'product_amount': product_amount
-        })
+        for id, quantity in cart.items():
+            product = get_object_or_404(Product, pk=id)
+            total += quantity * product.ticket_price
+            product_amount += 1
+            cart_items.append({
+                'id': id,
+                'quantity': quantity,
+                'product': product
+            })
 
     return {
-        'cart_items': cart,
+        'cart_items': cart_items,
         'total': total,
         'product_count': product_amount
     }
