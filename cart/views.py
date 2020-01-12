02919b7d8a907c3066d83d96dc80af9a3df9ca1f
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-01-12 21:45:49 +0000

    removed messages import

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib import messages
 from django.http import JsonResponse
 from competition.models import Competition
 from products.models import Product
