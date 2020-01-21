15ec54e3b7db6fe61cc622331a6a59e5ba26b566
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-01-21 00:48:25 +0000

    added docstring for home view

--- home/views.py
+++ home/views.py
@@ -4,6 +4,7 @@ from competition.models import Competition
 # Create your views here.
 
 def home(request):
+    """View for homepage with current competition ticket amount"""
     try:
         comp = Competition.objects.get(is_active=True)
         tickets_left = comp.tickets_left
