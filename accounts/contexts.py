eada61d75e760cec416b94d10b6ca0f00ddfe669
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-01-26 20:49:50 +0000

    improved check for incomplete profiles

--- accounts/contexts.py
+++ accounts/contexts.py
@@ -2,17 +2,23 @@ from .models import Profile
 
 def check_profile(request):
     """Check profile is complete"""
-
+    profile_incomplete = False
     if request.user.is_authenticated:
-
         profile = Profile.objects.get(id=request.user.id)
 
-        if profile.phone_number == 'Phone No.' and profile.county == "County":
-            profile_incomplete = True
-        else:
-            profile_incomplete = False
-    else:
-        profile_incomplete = False
+        model_check = [
+            profile.phone_number,
+            profile.address_line_1,
+            profile.town_city,
+            profile.county,
+            profile.country,
+            profile.postcode
+        ]
+
+        for item in model_check:
+            if item is None:
+                profile_incomplete = True
+                break
 
     return {
         'profile_incomplete': profile_incomplete
--- accounts/models.py
+++ accounts/models.py
@@ -6,10 +6,10 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     """Information relating to a single user not in User model"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=11, blank=False, default='Phone No.')
-    address_line_1 = models.CharField(max_length=40, blank=False, default='Address Line 1')
-    address_line_2 = models.CharField(max_length=40, blank=True, default='Address Line 2')
-    town_city = models.CharField(max_length=40, blank=False, default='Town / City')
-    county = models.CharField(max_length=40, blank=False, default='County')
-    country = models.CharField(max_length=40, blank=False, default='Country')
-    postcode = models.CharField(max_length=10, blank=False, default='Postcode')
+    phone_number = models.CharField(max_length=11, blank=True, default='')
+    address_line_1 = models.CharField(max_length=40, blank=True, default='')
+    address_line_2 = models.CharField(max_length=40, blank=True, default='')
+    town_city = models.CharField(max_length=40, blank=True, default='')
+    county = models.CharField(max_length=40, blank=True, default='')
+    country = models.CharField(max_length=40, blank=True, default='')
+    postcode = models.CharField(max_length=10, blank=True, default='')
