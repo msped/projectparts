9e3a0e8e7e7575e23421c36cd20cf217aefc3cda
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-03-01 23:33:13 +0000

    added dynamic years to payment form

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,12 +1,14 @@
+import datetime
 from django import forms
 
 class PaymentForm(forms.Form):
     """Payment form for stripe"""
+    now = datetime.datetime.now()
     MONTH_CHOICES = [(i, i) for i in range(1, 13)]
-    YEAR_CHOICES = [(i, i) for i in range(2016, 2075)]
+    YEAR_CHOICES = [(i, i) for i in range(now.year, now.year + 16)]
 
     credit_card_number = forms.CharField(label='Card number', required=False)
-    cvv = forms.CharField(label='Security code (CVV)', required=False)
+    cvv = forms.CharField(label='Security code (CVV)', max_length=3, required=False)
     expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
