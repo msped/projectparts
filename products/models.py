05148981b88ba8397b3e4ec5740199480f8d2586
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-03-03 23:15:15 +0000

    added brdige table for product fitments

--- products/models.py
+++ products/models.py
@@ -34,20 +34,24 @@ class Product(models.Model):
     ticket_price = models.DecimalField(max_digits=4, decimal_places=2)
     product_price = models.DecimalField(max_digits=7, decimal_places=2)
     product_link = models.URLField(default='#')
-    fits = models.ForeignKey(
-        Vehicle,
-        on_delete=models.DO_NOTHING,
-        null=True,
-        blank=True
-    )
     part_manufacturer = models.ForeignKey(
         Manufacturer,
         on_delete=models.DO_NOTHING
     )
-    fits_multiple = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['id']
 
     def __str__(self):
         return f"{self.part_manufacturer} {self.name}"
+
+class Fitments(models.Model):
+    """Bridge table to show ehich vehicles fit which products"""
+    products = models.ForeignKey(
+        Product,
+        on_delete=models.DO_NOTHING
+    )
+    vehicle = models.ForeignKey(
+        Vehicle,
+        on_delete=models.DO_NOTHING
+    )
