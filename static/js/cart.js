cef95c18aef30c40aee4a18c3ee90dc8ad236c73
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-03-11 17:32:50 +0000

    removed csrf token from data

--- static/js/cart.js
+++ static/js/cart.js
@@ -41,11 +41,37 @@ $(document).ready(function() {
         order_id = $(this).closest('td').find('.order-id').val();
         row = $(this).closest('tr');
         if (confirm("Do you wish to remove this item from your cart?")) {
+            function csrfSafeMethod(method) {
+                // these HTTP methods do not require CSRF protection
+                return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+            }
+            $.ajaxSetup({ 
+                beforeSend: function(xhr, settings) {
+                    function getCookie(name) {
+                        var cookieValue = null;
+                        if (document.cookie && document.cookie != '') {
+                            var cookies = document.cookie.split(';');
+                            for (var i = 0; i < cookies.length; i++) {
+                                var cookie = jQuery.trim(cookies[i]);
+                                // Does this cookie string begin with the name we want?
+                                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                                    break;
+                                }
+                            }
+                        }
+                        return cookieValue;
+                    }
+                    if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                        // Only send the token to relative URLs i.e. locally.
+                        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+                    }
+                } 
+            });
             $.ajax({
                 url: '/cart/remove/',
                 data: {
-                    'order_id': order_id,
-                    'csrfmiddlewaretoken': CSRF_TOKEN
+                    'order_id': order_id
                 },
                 type: 'POST',
                 success: function (d) {
