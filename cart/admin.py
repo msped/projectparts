5e32c8b0042a4ef4f55a81525163ea786f109433
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-03-19 21:26:03 +0000

    Made cart models read only in admin

--- cart/admin.py
+++ cart/admin.py
@@ -3,5 +3,28 @@ from .models import Order, OrderItem
 
 # Register your models here.
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+class OrderFilter(admin.ModelAdmin):
+    """Makes orders readonly in admin"""
+    readonly_fields = (
+        'user',
+        'related_competition',
+        'answer_correct', 
+        'items',
+        'order_date',
+        'payment_id',
+        'coupon'
+    )
+
+    filter_horizontal = ('items', )
+
+class OrderItemFilter(admin.ModelAdmin):
+    """Makes orderitems readonly in admin"""
+    readonly_fields = (
+        'user',
+        'product',
+        'quantity', 
+        'is_paid'
+    )
+
+admin.site.register(Order, OrderFilter)
+admin.site.register(OrderItem, OrderItemFilter)
