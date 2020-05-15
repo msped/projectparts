c53ff8f9523b1ccd80f9d97cf01c94f30a9ab678
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-05-15 20:21:22 +0100

    removed commented code

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -76,27 +76,7 @@ $(function() {
                         console.log(error.message);
                     }
                 }
-            })             
-            
-        //Stripe.createToken(card, function(status, response) {
-        //    if (status === 200) {
-        //        $("#credit-card-errors").hide();
-        //        $("#id_stripe_id").val(response.id);
-
-        //        $("#id_credit_card_number").removeAttr('name');
-        //        $("#id_cvv").removeAttr('name');
-        //        $("#id_expiry_month").removeAttr('name');
-        //        $("#id_expiry_year").removeAttr('name');
-
-        //        form.submit();
-        //    } else {
-        //        $('.loading').hide();
-        //        $('.submit').show();
-        //        $(".stripe-error-messages").text(response.error.message);
-        //        $(".credit-card-errors").show();
-        //        $("#validate_card_btn").attr("disabled", false);
-        //    }
-        //});
+            })
         return false;
         }
     });
