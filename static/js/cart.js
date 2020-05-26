318913d993f8d32080a62f5183f351e75df67177
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-05-26 20:29:00 +0100

    removed csrf cooke function

--- static/js/cart.js
+++ static/js/cart.js
@@ -40,34 +40,6 @@ $(document).ready(function() {
     $('td').on('click', '.remove-item', function (e) {
         order_id = $(this).closest('td').find('.order-id').val();
         row = $(this).closest('tr');
-        if (confirm("Do you wish to remove this item from your cart?")) {
-            function csrfSafeMethod(method) {
-                // these HTTP methods do not require CSRF protection
-                return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-            }
-            $.ajaxSetup({ 
-                beforeSend: function(xhr, settings) {
-                    function getCookie(name) {
-                        var cookieValue = null;
-                        if (document.cookie && document.cookie != '') {
-                            var cookies = document.cookie.split(';');
-                            for (var i = 0; i < cookies.length; i++) {
-                                var cookie = jQuery.trim(cookies[i]);
-                                // Does this cookie string begin with the name we want?
-                                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                                    break;
-                                }
-                            }
-                        }
-                        return cookieValue;
-                    }
-                    if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-                        // Only send the token to relative URLs i.e. locally.
-                        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-                    }
-                } 
-            });
             $.ajax({
                 url: '/cart/remove/',
                 data: {
--- static/js/stripe.js
+++ static/js/stripe.js
@@ -18,33 +18,6 @@ $(function() {
         if ($("#user-answer option:selected").text() !== "Please select an option"){      
             $('.submit').hide();
             $('.loading').show();
-            function csrfSafeMethod(method) {
-                // these HTTP methods do not require CSRF protection
-                return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-            }
-            $.ajaxSetup({ 
-                beforeSend: function(xhr, settings) {
-                    function getCookie(name) {
-                        var cookieValue = null;
-                        if (document.cookie && document.cookie != '') {
-                            var cookies = document.cookie.split(';');
-                            for (var i = 0; i < cookies.length; i++) {
-                                var cookie = jQuery.trim(cookies[i]);
-                                // Does this cookie string begin with the name we want?
-                                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                                    break;
-                                }
-                            }
-                        }
-                        return cookieValue;
-                    }
-                    if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-                        // Only send the token to relative URLs i.e. locally.
-                        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-                    }
-                } 
-           });
             $.ajax({
                 type: "POST",
                 url: "/checkout/",
@@ -57,7 +30,6 @@ $(function() {
                         payment_method: {
                             card: card
                         }
-                       
                         }).then(function (result) {
                             if (result.error) {
                                 $('.loading').hide();
