34d62c82038f45574e1325792fc5b518627c2414
Matt Edwards <mattsp.edwards@googlemail.com>
2021-07-17 20:14:17 +0100

    changed home view to a cbv

--- home/views.py
+++ home/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
+from django.views import View
 from competition.models import Competition
 
 # Create your views here.
 
-def home(request):
-    """View for homepage with current competition ticket amount"""
-    try:
-        comp = Competition.objects.get(is_active=True)
-        tickets_left = comp.tickets_left
-    except Competition.DoesNotExist:
-        tickets_left = "No Competition Active"
+class Home(View):
+    
+    def get(self, request):
+        try:
+            comp = Competition.objects.get(is_active=True)
+            tickets_left = comp.tickets_left
+        except Competition.DoesNotExist:
+            tickets_left = "No Competition Active"
 
-    content = {
-        'tickets_left': tickets_left
-    }
-    return render(request, 'home.html', content)
+        content = {
+            'tickets_left': tickets_left
+        }
+        return render(request, 'home.html', content)
