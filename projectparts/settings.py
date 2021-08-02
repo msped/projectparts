201fe8bb9ce2f5a7cd8790cd00f284464879eaf4
Matt Edwards <mattsp.edwards@googlemail.com>
2021-08-02 20:40:05 +0100

    change if statement to be dependant on debug

--- projectparts/settings.py
+++ projectparts/settings.py
@@ -162,7 +162,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if "DATABASE_URL" in os.environ:
+if DEBUG == False:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
